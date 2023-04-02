d59fe2f2a8cbd4403c6d3d5df90e9f0dbb8bc09b
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-04-02 20:19:36 -0300

    Add main

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,32 @@
 import Head from "next/head";
+import styled from "styled-components";
 import { Banner } from "@/components";
+import { Wrapper } from "@/css";
 
 const Home = () => {
   return (
-    <>
+    <Container>
       <Head>
         <title>CV | Alvaro Aguirre</title>
       </Head>
       <Banner />
-    </>
+      <Main>
+        <Wrapper>asd</Wrapper>
+      </Main>
+    </Container>
   );
 };
 
 export default Home;
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+`;
+
+const Main = styled.main`
+  flex: 1;
+  background-color: ${({ theme }) => theme.assets.bgPrimary};
+`;
