be4f989820dcc2e4355442e04f4d1b605fd7aab9
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-04-02 20:05:48 -0300

    Banner

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,22 +1,40 @@
 import React, { FunctionComponent } from "react";
 import Image from "next/image";
 import styled from "styled-components";
+import { Wrapper } from "@/css";
 import { originalImageWitdth } from "@/css/commonStyles";
 
 export const Banner: FunctionComponent = () => {
   return (
     <Container>
-      <ImageWrapper>
-        <Image src="/images/avatar.jpg" layout="fill" alt="Alvaro Aguirre Avatar" />
-      </ImageWrapper>
-      <div>hola</div>
+      <Wrapper>
+        <InnerContainer>
+          <ImageWrapper>
+            <Image src="/images/avatar.jpg" layout="fill" alt="Alvaro Aguirre Avatar" />
+          </ImageWrapper>
+          <TextContainer>
+            <Title>Alvaro Aguirre</Title>
+            <div>
+              <Subtitle>Frontend developer</Subtitle>
+              <Subtitle>Degree in Computer Science</Subtitle>
+            </div>
+            <Location>Buenos Aires, Argentina</Location>
+          </TextContainer>
+        </InnerContainer>
+      </Wrapper>
     </Container>
   );
 };
 
 const Container = styled.div`
+  background-color: ${({ theme }) => theme.colors.grey[8]};
+  padding: ${({ theme }) => theme.spacing[6]} 0;
+`;
+
+const InnerContainer = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
+  align-items: center;
 `;
 
 const ImageWrapper = styled.div`
@@ -25,4 +43,29 @@ const ImageWrapper = styled.div`
   ${originalImageWitdth};
   border-radius: ${({ theme }) => theme.borderRadius.full};
   overflow: hidden;
+  > span {
+    margin-top: -1rem !important;
+  }
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing[2]};
+`;
+
+const Title = styled.h1`
+  color: ${({ theme }) => theme.colors.grey[2]};
+  font-size: ${({ theme }) => theme.typography.fontSizes["3xl"]};
+`;
+
+const Subtitle = styled.h3`
+  color: ${({ theme }) => theme.colors.grey[5]};
+  font-size: ${({ theme }) => theme.typography.fontSizes.lg};
+  font-weight: ${({ theme }) => theme.typography.fontWeights.normal};
+`;
+
+const Location = styled.span`
+  color: ${({ theme }) => theme.colors.grey[5]};
+  font-size: ${({ theme }) => theme.typography.fontSizes.md};
 `;
