f25451ecaaaf2df962fd30a3df2b8a182d3dfe88
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2024-01-18 00:22:33 -0300

    production text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import { Wrapper } from "@/components";
 import { H1 } from "@/components/ui";
 
 export default function Home() {
+  if (process.env.NODE_ENV === "production") {
+    return <H1>{"In progress :)"}</H1>;
+  }
+
   return (
     <div className="my-16">
       <Wrapper>
