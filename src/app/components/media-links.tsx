02b54d77ada1f697b2efc82cfaae0b3daccaa9b5
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2024-01-20 13:44:26 -0300

    add target blank to media links

--- src/app/components/media-links.tsx
+++ src/app/components/media-links.tsx
@@ -9,6 +9,7 @@ const MediaLinks = () => {
             key={url}
             href={url}
             className="rounded-md border p-2 transition-colors hover:bg-hover-primary"
+            target="_blank"
           >
             <Icon size={16} />
           </a>
