0d97f6469d96f66af80716139513a5aaf232d5cd
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2024-01-20 13:37:13 -0300

    links on contact

--- src/app/components/media-links.print.tsx
+++ src/app/components/media-links.print.tsx
@@ -1,20 +1,19 @@
 import { Section } from "@/components";
 import { H2 } from "@/components/ui";
+import { links } from "@/config/links";
 
 const MediaLinksPrint = () => {
   return (
     <Section className="hidden print:block">
       <H2>Contact</H2>
-      <div className="flex justify-between font-mono">
-        <div className="flex flex-col">
-          <span>1aguirrealvaro@gmail.com</span>
-          <span>+54 11 6324 7350</span>
-        </div>
-        <div className="flex flex-col">
-          <span>aguirrealvaro.dev</span>
-          <span>github.com/aguirrealvaro</span>
-          <span>linkedin.com/in/aguirrealvaro</span>
-        </div>
+      <div className="grid grid-cols-2">
+        {links.map(({ url, cleanUrl }) => {
+          return (
+            <span key={url} className="font-mono">
+              {cleanUrl}
+            </span>
+          );
+        })}
       </div>
     </Section>
   );
